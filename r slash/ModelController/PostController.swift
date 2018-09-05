8b0edcf5366726e72572669a366d80d87e976bb6
Jason Goodney <Jason.Goodney@gmail.com>
2018-09-04 20:19:29 -0600

    Fetches and populates tableView with reddit json

---
+++ r slash/ModelController/PostController.swift
@@ -0,0 +1,9 @@
+//
+//  PostController.swift
+//  r slash
+//
+//  Created by Jason Goodney on 9/4/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import Foundation
---
+++ r slash/View/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  r slash
+//
+//  Created by Jason Goodney on 9/4/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
